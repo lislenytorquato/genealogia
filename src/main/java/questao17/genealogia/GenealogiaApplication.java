5fa508f4ca169d8a169720bc8df0204de5ee5d11
lislenytorquato <lislenytorquato19@gmail.com>
2025-05-28 11:41:55 -0300

    starting project

---
+++ src/main/java/questao17/genealogia/GenealogiaApplication.java
@@ -0,0 +1,13 @@
+package questao17.genealogia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogiaApplication.class, args);
+	}
+
+}
