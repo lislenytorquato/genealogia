4b91396f6c3ab520f2d75427898d4fd5c0ac321d
Lisleny Silva <lisleny.silva@dbserver.com.br>
2025-05-28 11:41:55 -0300

    starting project

---
+++ src/test/java/questao17/genealogia/GenealogiaApplicationTests.java
@@ -0,0 +1,13 @@
+package questao17.genealogia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenealogiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
